5a140f096efe02dbf3cac8bfae64680399ed4ebe
jefe33 <80694@student.pb.edu.pl>
2022-06-05 00:23:02 +0200

    init

---
+++ Models/TaskModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TaskManager.Models
+{
+    [Table("Tasks")]
+    public class TaskModel
+    {
+        [Key]
+        public int TaskId { get; set; }
+
+        [DisplayName("Nazwa")]
+        [Required(ErrorMessage = "Pole nazwa jest wymagane!")]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        [DisplayName("Opis")]
+        [MaxLength(200)]
+        public string Description { get; set; }
+        public bool Done { get; set; }
+    }
+}
